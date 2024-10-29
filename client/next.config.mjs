cd74ab465f1613461e724326328826c35e9bce8a
Surafel Melese Tebeje <58382595+surafelmelese@users.noreply.github.com>
2024-10-29 15:23:16 +0300

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['pizza-ordering-service-ivory.vercel.app', 'assets'], // Corrected domains
+  },
+    eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
