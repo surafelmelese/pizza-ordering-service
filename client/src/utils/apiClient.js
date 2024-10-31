1f2a712674eef9d653dc6ddbe5a4946c13d7bd48
Surafel Melese Tebeje <58382595+surafelmelese@users.noreply.github.com>
2024-10-31 16:57:01 +0300

    Update apiClient.js

--- client/src/utils/apiClient.js
+++ client/src/utils/apiClient.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //https://pizza-ordering-service-1.onrender.com
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/' ,// Base API
+  baseURL: 'https://garageapp-cp16.onrender.com' ,// Base API
   headers: {
     'Content-Type': 'application/json',
   },
